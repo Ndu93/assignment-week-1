e9fc422382c0badabe459993b625feb960a0cd87
Ndu93 <danielndu93@gmail.com>
2024-11-08 00:11:46 +0100

    first commit

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# user information
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+# Create and print personalized message
+print(f"\nHello {name}! It's great to meet someone who's {age} years old and who live in {location}.")
+print(f"I hope you're having a wonderful day in {location}!")
